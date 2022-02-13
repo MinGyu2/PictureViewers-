8ebc27b6168fd74b52153dbd59328edef193a474
MinQ <MinQ@DESKTOP-1I1G0BH>
2022-02-13 20:05:04 +0900

    윈도우 사진 표시 예제 만들어봄

--- PictureViewers/Form1.Designer.cs
+++ PictureViewers/Form1.Designer.cs
@@ -24,21 +24,118 @@
         /// </summary>
         private void InitializeComponent() {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.showBT = new System.Windows.Forms.Button();
+            this.clearBT = new System.Windows.Forms.Button();
+            this.backgroundBT = new System.Windows.Forms.Button();
+            this.closeBT = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(649, 425);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 376);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 385);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "늘리기";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.showBT);
+            this.flowLayoutPanel1.Controls.Add(this.clearBT);
+            this.flowLayoutPanel1.Controls.Add(this.backgroundBT);
+            this.flowLayoutPanel1.Controls.Add(this.closeBT);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(197, 385);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(449, 37);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // showBT
+            // 
+            this.showBT.Location = new System.Drawing.Point(371, 3);
+            this.showBT.Name = "showBT";
+            this.showBT.Size = new System.Drawing.Size(75, 23);
+            this.showBT.TabIndex = 0;
+            this.showBT.Text = "사진 표시";
+            this.showBT.UseVisualStyleBackColor = true;
+            this.showBT.Click += new System.EventHandler(this.showBT_Click);
+            // 
+            // clearBT
+            // 
+            this.clearBT.Location = new System.Drawing.Point(280, 3);
+            this.clearBT.Name = "clearBT";
+            this.clearBT.Size = new System.Drawing.Size(85, 23);
+            this.clearBT.TabIndex = 1;
+            this.clearBT.Text = "사진 지우기";
+            this.clearBT.UseVisualStyleBackColor = true;
+            this.clearBT.Click += new System.EventHandler(this.clearBT_Click);
+            // 
+            // backgroundBT
+            // 
+            this.backgroundBT.Location = new System.Drawing.Point(176, 3);
+            this.backgroundBT.Name = "backgroundBT";
+            this.backgroundBT.Size = new System.Drawing.Size(98, 23);
+            this.backgroundBT.TabIndex = 2;
+            this.backgroundBT.Text = "배경색 설정";
+            this.backgroundBT.UseVisualStyleBackColor = true;
+            this.backgroundBT.Click += new System.EventHandler(this.backgroundBT_Click);
+            // 
+            // closeBT
+            // 
+            this.closeBT.Location = new System.Drawing.Point(95, 3);
+            this.closeBT.Name = "closeBT";
+            this.closeBT.Size = new System.Drawing.Size(75, 23);
+            this.closeBT.TabIndex = 3;
+            this.closeBT.Text = "닫기";
+            this.closeBT.UseVisualStyleBackColor = true;
+            this.closeBT.Click += new System.EventHandler(this.closeBT_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
             // 
             // Form1
             // 
@@ -48,6 +145,10 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -55,6 +156,15 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button showBT;
+        private System.Windows.Forms.Button clearBT;
+        private System.Windows.Forms.Button backgroundBT;
+        private System.Windows.Forms.Button closeBT;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- PictureViewers/Form1.cs
+++ PictureViewers/Form1.cs
@@ -13,5 +13,36 @@ namespace PictureViewers {
         public Form1() {
             InitializeComponent();
         }
+
+
+        private void showBT_Click(object sender, EventArgs e) {
+            // 사진 표시 버튼을 누르면 파일 목록을 보여준다. 사진을 클릭하고 ok 하면 사진을 load 합니다.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) { 
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearBT_Click(object sender, EventArgs e) {
+            pictureBox1.Image = null; 
+        }
+
+        private void backgroundBT_Click(object sender, EventArgs e) {
+            if(colorDialog1.ShowDialog() == DialogResult.OK) {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void closeBT_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            if (checkBox1.Checked) {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            } else {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+
+            }
+        }
     }
 }
